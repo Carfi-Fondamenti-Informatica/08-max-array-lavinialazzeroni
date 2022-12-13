b0aa5e691b282a7cde3f1ed9de20532c6cc3110e
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-12-13 08:34:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
+float funzione(float numeri[], int dimensione){
+float max = numeri[0];
+for (int i=0; i < dimensione; i++){
+if (max < numeri[i]){
+max = numeri[i];
+}
+}
+return max;
+}
 
